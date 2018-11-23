44254acef8ddaf052fa0080a1dd3da5521ba76b7
Melih YILDIZ <mzyil12@gmail.com>
2018-11-23 11:07:21 +0300

    Delete unnecessary files

--- built/mochaVErrorReporter.d.ts
+++
@@ -1,8 +0,0 @@
-import { Runner } from "mocha";
-export declare class MochaVErrorReporter extends Mocha.reporters.Base {
-    private static _DEFAULT_INDENT;
-    private static _REGEX_REMOVE_REPEATED_MESSAGE;
-    private static _REGEX_REPLACE_WITH_INDENT;
-    constructor(runner: Runner);
-    static fullStack(error: any, indent?: string, indentLevel?: number): any;
-}
