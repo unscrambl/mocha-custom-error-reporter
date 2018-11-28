26f01713c455cf0ba9274767c1ad7c621bbf89af
Melih YILDIZ <mzyil12@gmail.com>
2018-11-28 19:51:07 +0300

    Refactor namings

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -5,7 +5,7 @@ import { MochaVErrorReporter } from "../src/mochaVErrorReporter";
 describe("MochaVErrorReporter", () =>
 {
 
-    class ErrorWithExternalStackTrace extends Error
+    class ErrorWithExampleStackTrace extends Error
     {
         stackTraceString: string;
         stackTrace = () => {
@@ -20,17 +20,17 @@ describe("MochaVErrorReporter", () =>
 
     }
 
-    it("outputs the correct output when there is a 'stackTrace' function",  () =>
+    it("outputs the correct information when there is a 'stackTrace' function",  () =>
     {
-        const testStackMessage = "external stack trace";
-        const sampleError = new ErrorWithExternalStackTrace(testStackMessage);
+        const testStackMessage = "example stack trace";
+        const sampleError = new ErrorWithExampleStackTrace(testStackMessage);
         const fnOutput = MochaVErrorReporter.fullStack(sampleError);
         expect(fnOutput).to.equal(testStackMessage); 
     });
 
-    it("outputs the correct output when there is not a 'stackTrace' function",  () =>
+    it("outputs the correct information when there is not a 'stackTrace' function",  () =>
     {
-        const testStackMessage = "external stack trace";
+        const testStackMessage = "example stack trace";
         const sampleError = new Error(testStackMessage);
         const fnOutput = MochaVErrorReporter.fullStack(sampleError);
         expect(fnOutput).to.not.equal(testStackMessage);
