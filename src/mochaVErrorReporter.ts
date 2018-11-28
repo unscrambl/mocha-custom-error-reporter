ddf1191b038887646f4b44865f2ed7f8a8d482f2
Melih YILDIZ <mzyil12@gmail.com>
2018-11-28 17:00:10 +0300

    Adjust the package to use a stackTrace function instead of a full implementation which would be subject to changes

--- src/mochaVErrorReporter.ts
+++ src/mochaVErrorReporter.ts
@@ -1,57 +1,19 @@
-import { reporters as MochaReporters, Runner, utils as MochaUtils } from "mocha";
-import { VError } from "verror";
+import { reporters, Runner } from "mocha";
 
-export class MochaVErrorReporter extends MochaReporters.Base
+export class MochaVErrorReporter extends reporters.Base
 {
-    private static readonly _DEFAULT_INDENT: string = "    ";
-    private static readonly _REMOVE_REPEATED_MESSAGE_REGEX: RegExp = /caused by.+\n/;
-    private static readonly _REPLACE_WITH_INDENT_REGEX: RegExp = /\n/g;
-    private static readonly _MOCHA_STACK_FILTER = MochaUtils.stackTraceFilter();
-
     constructor(runner: Runner)
     {
         super(runner);
-        runner.on("fail", (test, err: Error) =>
+        runner.on("fail", (test, error: Error) =>
         {
-            err.stack = MochaVErrorReporter._MOCHA_STACK_FILTER(
-                MochaVErrorReporter.fullStack(err));
+            error.stack = MochaVErrorReporter.fullStack(error);
         });
     }
 
-    public static fullStack(error: Error, indent: string = MochaVErrorReporter._DEFAULT_INDENT): string
+    public static fullStack(error: any): string
     {
-        return MochaVErrorReporter.fullStackImpl(error, indent, 1);
+        return (typeof error.stackTrace === "function") ? error.stackTrace() : error.stack;
     }
 
-    private static fullStackImpl(error, indent = MochaVErrorReporter._DEFAULT_INDENT, indentationLevel = 1)
-    {
-        if (typeof error.errors === "function")
-        {
-            const suppressedErrors = error.errors();
-            let message = `MultiError: with ${suppressedErrors.length} suppressed errors:`;
-            let index = 1;
-            for (const suppressedError of suppressedErrors)
-            {
-                message += `\n${indent.repeat(indentationLevel)}`;
-                message += `error #${index}/${suppressedErrors.length}: `;
-                message += `${MochaVErrorReporter.fullStackImpl(suppressedError, indent, indentationLevel + 1)}`;
-                index++;
-            }
-            return message;
-        }
-        if (typeof error.cause === "function")
-        {
-            const cause = error.cause();
-            error.stack = error.stack.replace(MochaVErrorReporter._REMOVE_REPEATED_MESSAGE_REGEX, "\n");
-            if (cause)
-            {
-                return `${error.stack.replace(MochaVErrorReporter._REPLACE_WITH_INDENT_REGEX, "\n"
-                    + indent.repeat(indentationLevel))}`
-                    + `\n${indent.repeat(indentationLevel)}caused by, `
-                    + `${MochaVErrorReporter.fullStackImpl(cause, indent, indentationLevel + 1)}`;
-            }
-        }
-        return VError.fullStack(error).replace(MochaVErrorReporter._REPLACE_WITH_INDENT_REGEX, "\n"
-            + indent.repeat(indentationLevel));
-    }
 }
--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,39 +1,38 @@
 
 import { expect } from "chai";
-import * as VError from "verror";
 import { MochaVErrorReporter } from "../src/mochaVErrorReporter";
 
 describe("MochaVErrorReporter", () =>
 {
-    const indentChars = "    ";
-    const sampleError = new VError.MultiError(
-        [
-            new VError.WError(
-                new VError.MultiError(
-                    [
-                        new VError.WError(
-                            new VError.WError("a nested error inside a nested error"),
-                            "a nested error"
-                        ),
-                        new Error("another nested error")
-                    ]
-                ),
-                "encapsulating a multierror"
-            ),
-            new VError.WError("exists at the same level with encapsulator")
-        ]
-    );
 
-    const fnOutput = MochaVErrorReporter.fullStack(sampleError, indentChars).replace(/\n.*\s{4}at\s.*/g, "");
-
-    it("recursively outputs errors with correct indentation",  () =>
+    class ErrorWithExternalStackTrace extends Error
     {
-        const expectedOutput =  "MultiError: with 2 suppressed errors:\n" +
-            "    error #1/2: WError: encapsulating a multierror\n" +
-            "        caused by, MultiError: with 2 suppressed errors:\n" +
-            "            error #1/2: WError: a nested error\n" +
-            "                caused by, WError: a nested error inside a nested error\n" +
-            "            error #2/2: Error: another nested error\n" +
-            "    error #2/2: WError: exists at the same level with encapsulator";
-        expect(fnOutput).to.equal(expectedOutput); });
+        stackTraceString: string;
+        stackTrace = () => {
+            return this.stackTraceString;
+        }
+        
+        constructor(stackTrace: string)
+        {
+            super("example error");
+            this.stackTraceString = stackTrace;
+        }
+
+    }
+
+    it("outputs the correct output when there is a 'stackTrace' function",  () =>
+    {
+        const testStackMessage = "external stack trace";
+        const sampleError = new ErrorWithExternalStackTrace(testStackMessage);
+        const fnOutput = MochaVErrorReporter.fullStack(sampleError);
+        expect(fnOutput).to.equal(testStackMessage); 
+    });
+
+    it("outputs the correct output when there is not a 'stackTrace' function",  () =>
+    {
+        const testStackMessage = "external stack trace";
+        const sampleError = new Error(testStackMessage);
+        const fnOutput = MochaVErrorReporter.fullStack(sampleError);
+        expect(fnOutput).to.not.equal(testStackMessage);
+    });
 });
