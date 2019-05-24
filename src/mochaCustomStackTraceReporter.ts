fb313b820014f8fdaaabf46b2bb9df6ad3012ded
S. Ahmet Bolac <ahmet@unscrambl.com>
2019-05-24 18:10:18 +0300

    Update for PR review comments

--- src/mochaCustomStackTraceReporter.ts
+++ src/mochaCustomStackTraceReporter.ts
@@ -9,14 +9,14 @@ class MochaCustomStackTraceReporter extends reporters.Base
         {
             error.stack = MochaCustomStackTraceReporter.stackTrace(error);
         });
-        
+
         runner.on("start", () => {
             process.on("unhandledRejection", (reason, promise) => {
-                console.log("ERROR: Unhandled Promise Rejection at: %s", reason.stack || reason);
-                process.exit(100);
-            });  
-        });        
-   }
+                console.log("ERROR: the execution failed due to an unhandled promise rejection, details: %s", reason.stack || reason);
+                process.exit(process.exitCode | 100);
+            });
+        });
+    }
 
     public static stackTrace(error: any): string
     {
